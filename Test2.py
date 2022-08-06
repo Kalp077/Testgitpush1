771c436448d2d233e85a1d4d9ea40e558323202c
Kalpesh <kshewale077@gmail.com>
2022-08-06 18:14:30 +0530

    heyfhfdgh

--- Test2.py
+++ Test2.py
@@ -10,6 +10,40 @@ data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
         'addr' : ['blr' , 'blr' , 'blr' , 'mum']
     }
 
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+
+
+
+
 print(data)
 
 
