678b4858d9586e61442fb0f15e0c7c49e9bc2d15
Kalpesh <kshewale077@gmail.com>
2022-08-06 18:05:41 +0530

    hey

---
+++ Test2.py
@@ -0,0 +1,16 @@
+
+
+
+print("hey how are you?")
+
+
+data = {'name' : ['sudh' , 'krish' , 'hitesh' , 'tulesko'],
+        'salary' : [100 , 200 , 300 , 400],
+        'mail_id' : ['sudh@ineuron.ai' , 'krish@ineuron.ai' , 'hitesh@ineuron.ai' , 'tulesko@ineuron.ai'],
+        'addr' : ['blr' , 'blr' , 'blr' , 'mum']
+    }
+
+print(data)
+
+
+
